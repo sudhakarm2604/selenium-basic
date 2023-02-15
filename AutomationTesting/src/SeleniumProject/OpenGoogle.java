4d38808d1d1697718dba22afc30eb3104f783a9a
Sudhakar <Sudhakar@Sudhakar>
2023-02-15 12:56:02 +0530

    Added the search feature

--- AutomationTesting/src/SeleniumProject/OpenGoogle.java
+++ AutomationTesting/src/SeleniumProject/OpenGoogle.java
@@ -1,5 +1,6 @@
 package SeleniumProject;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -11,7 +12,8 @@ public class OpenGoogle {
 		System.setProperty("webdriver.chrome.driver","C:\\Automation Testing\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com/");
-		Thread.sleep(5000);
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//input[@title='Search']")).sendKeys("Hrithik roshan");
         driver.close();	
 	}
 
