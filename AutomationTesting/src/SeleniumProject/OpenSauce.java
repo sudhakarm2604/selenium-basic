1229f0c4b70974812fa29a715f54ff234b6b5c6a
Sudhakar <Sudhakar@Sudhakar>
2023-02-15 12:41:30 +0530

    Added the Opensauce test

---
+++ AutomationTesting/src/SeleniumProject/OpenSauce.java
@@ -0,0 +1,22 @@
+package SeleniumProject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenSauce {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver","C:\\Automation Testing\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.saucedemo.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.id("login-button")).click();
+        driver.findElement(By.id("react-burger-menu-btn")).click();
+        driver.close();
+
+	}
+
+}
